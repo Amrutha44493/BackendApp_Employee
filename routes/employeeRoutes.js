b7665665faa6ff813c8fee39901652a297175c6c
amrutha <amrutha@cenddle.com>
2025-03-12 06:08:56 +0530

    updation

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -13,7 +13,7 @@ router.get('/employeelist', async (req, res) => {
         console.log("Employees found:", data);
         res.status(200).json(data); 
     } catch (error) {
-        console.error("Error fetching employees:", error);
+        console.error("Error fetching employees", error);
         res.status(500).send('Internal Server Error');
     }
 });
@@ -27,10 +27,9 @@ router.get('/employeelist/:id', async (req, res) => {
         if (!employee) {
             return res.status(404).send('Employee not found');
         }
-
         res.status(200).json(employee); 
     } catch (error) {
-        console.error("Error:", error);
+        console.error("Error", error);
         res.status(500).send('Internal Server Error');
     }
 });
@@ -41,35 +40,45 @@ router.post('/employeelist', async (req, res) => {
         const item = req.body;
         const data1 = new empModel(item);
         await data1.save();
-        res.status(201).send(' created successfully');
+        res.status(200).json({ message: "Inserted successful", data1 });
+
     } catch (error) {
         console.error("Error:", error);
-        res.status(500).send('Internal Server Error');
+        res.status(500).send(' Insertion Failed');
     }
 });
 
 // PUT operation
-router.put('/employeelist', async (req, res) => {
+router.put('/employeelist/', async (req, res) => {
     try {
-        const id = req.params.id; 
-        await empModel.findByIdAndUpdate(id, req.body);
-        res.status(200).send(' updated successfully');
-    } catch (error) {
-        console.error("Error:", error);
-        res.status(500).send('Internal Server Error');
-    }
+        if (!req.body._id) {
+          return res.status(400).json({ error: "Employee ID required!" });
+        }
+        const updatedEmployee = await empModel.findOneAndUpdate(
+          { _id: req.body._id },
+          req.body,
+          { new: true }
+        );
+    
+        if (!updatedEmployee) {
+          return res.status(404).json({ error: "Employee not found" });
+        }
+        res.status(200).json({ message: "Update successful", updatedEmployee });
+      } catch (error) {
+        res.status(500).send("Update failed.");
+      }
 });
-
 // DELETE operation
 router.delete('/employeelist/:id', async (req, res) => {
     try {
         const id = req.params.id;
         await empModel.findByIdAndDelete(id);
-        res.status(200).send(' removed successfully');
+        res.status(200).json({ message: "deleted successful"});
     } catch (error) {
         console.error("Error:", error);
-        res.status(500).send('Internal Server Error');
+        res.status(500).send('Unsuccessful');
     }
 });
+
 
 module.exports = router;
